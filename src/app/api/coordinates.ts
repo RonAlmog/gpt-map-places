bee1c16facb2e308c3ad1dbb8ddcfdf904372f5e
Ron Almog <ron8132@gmail.com>
2025-09-23 11:14:15 -0400

    add api anedpoint

--- src/app/api/coordinates.ts
+++ src/app/api/coordinates.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const openai = new OpenAI();
 
-export default async function handler(req: Request, res: any) {
+export default async function handler(req: any, res: any) {
   try {
     const gpt4Completion = await openai.chat.completions.create({
       model: "gpt-4",
@@ -15,7 +15,21 @@ export default async function handler(req: Request, res: any) {
           content:
             "You only return in JSON a coordinates key with value in this format [43.6426, -79.3871], then a title of the location with a title key",
         },
+        { role: "user", content: req.body.value },
       ],
     });
-  } catch (error) {}
+
+    // get the text response
+    const responseText = gpt4Completion.choices[0]?.message?.content;
+    // check if it looks like json
+    if (responseText && responseText[0] === "{") {
+      const jsonResult = JSON.parse(responseText);
+      res.status(200).json(jsonResult);
+      console.log({ jsonResult });
+    } else {
+      res.status(200).json({ tryAgain: true });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
